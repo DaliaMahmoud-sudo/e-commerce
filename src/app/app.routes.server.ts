93eeae653b58768a47e4e9368f25f098bb25a99e
DaliaMahmoud-sudo <dalia.m3544214@gmail.com>
2026-03-28 14:10:52 +0200

    modify approutes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,30 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Routes with parameters must be Client or Server rendered 
+  // to avoid the getPrerenderParams error
+  {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout', // If this has a :cartId in the future, keep it Client
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'allorders',
+    renderMode: RenderMode.Client
+  },
+  // All other static pages (Home, Shop, Login, etc.) 
+  // can be Prerendered for better performance
   {
     path: '**',
     renderMode: RenderMode.Prerender
